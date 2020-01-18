a2a809d467974e770ab6d4b76a4cac773075e72d
severedsolo <severedsolo@users.noreply.github.com>
2020-01-18 10:50:43 +0000

    FlightTracker 3.0 Update

--- MemorialWall/MemorialWall.cs
+++ MemorialWall/MemorialWall.cs
@@ -10,53 +10,53 @@ namespace MemorialWall
     [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
     public class MemorialWall : MonoBehaviour
     {
-        private IEnumerable<CrewListItem> _crewItemContainers;
-        private bool _astronautComplexSpawned;
-        private StringBuilder _label = new StringBuilder();
-        private ActiveFlightTracker _tracker;
-        private bool _foundDeadKerbal;
+        private IEnumerable<CrewListItem> crewItemContainers;
+        private bool astronautComplexSpawned;
+        private readonly StringBuilder label = new StringBuilder();
+        private FlightTrackerApi tracker;
+        private bool foundDeadKerbal;
 
    private void Start()
         {
             GameEvents.onGUIAstronautComplexSpawn.Add(AstronautComplexSpawned);
             GameEvents.onGUIAstronautComplexDespawn.Add(AstronautComplexDespawned);
-            _tracker = ActiveFlightTracker.instance;
+            tracker = FlightTrackerApi.Instance;
             Debug.Log("[MemorialWall]: MemorialWall has registered events");
         }
 
         private void AstronautComplexDespawned()
         {
-            _astronautComplexSpawned = false;
-            _foundDeadKerbal = false;
+            astronautComplexSpawned = false;
+            foundDeadKerbal = false;
             Debug.Log("[MemorialWall]: Astronaut Complex despawned");
         }
 
         private void AstronautComplexSpawned()
         {
-            _astronautComplexSpawned = true;
+            astronautComplexSpawned = true;
             Debug.Log("[MemorialWall]: Astronaut Complex spawned");
         }
 
         private void LateUpdate()
         {
-            if (!_astronautComplexSpawned || _foundDeadKerbal) return;
+            if (!astronautComplexSpawned || foundDeadKerbal) return;
             // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
-            _crewItemContainers = FindObjectsOfType<CrewListItem>();
-            for (int i = 0; i < _crewItemContainers.Count(); i++)
+            crewItemContainers = FindObjectsOfType<CrewListItem>();
+            for (int i = 0; i < crewItemContainers.Count(); i++)
             {
-                CrewListItem crewContainer = _crewItemContainers.ElementAt(i);
+                CrewListItem crewContainer = crewItemContainers.ElementAt(i);
                 ProtoCrewMember p = crewContainer.GetCrewRef();
                 if (p.rosterStatus != ProtoCrewMember.RosterStatus.Dead) continue;
                 crewContainer.SetName(p.name + " (Deceased)");
                 // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
-                if (!_foundDeadKerbal) Debug.Log("[MemorialWall]: Overwriting AstronautComplexGUI");
-                _foundDeadKerbal = true;
-                string flights = _tracker.GetNumberOfFlights(p.name).ToString();
-                string hours = _tracker.ConvertUtToString(_tracker.GetRecordedMissionTimeSeconds(p.name));
-                string worldFirsts = _tracker.GetNumberOfWorldFirsts(p.name).ToString();
-                _label.Length = 0;
-                _label.Append("Flights: " + flights + " | Hours: " + hours + " | World Firsts: " + worldFirsts);
-                crewContainer.SetLabel(_label.ToString());
+                if (!foundDeadKerbal) Debug.Log("[MemorialWall]: Overwriting AstronautComplexGUI");
+                foundDeadKerbal = true;
+                string flights = tracker.GetNumberOfFlights(p.name).ToString();
+                string hours = tracker.ConvertUtToString(tracker.GetRecordedMissionTimeSeconds(p.name));
+                string worldFirsts = tracker.GetNumberOfWorldFirsts(p.name).ToString();
+                label.Length = 0;
+                label.Append("Flights: " + flights + " | Hours: " + hours + " | World Firsts: " + worldFirsts);
+                crewContainer.SetLabel(label.ToString());
             }
         }
     }
